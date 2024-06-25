4d784306a04c2f173b2be049c6dea1c8b717ebaf
Dinesh Kumar <dinesh@vegasega.com>
2024-06-25 18:15:40 +0530

    sort

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,12 +12,12 @@ plugins {
 
 android {
     namespace = "com.vegasega.amnews"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.vegasega.amnews"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
 
